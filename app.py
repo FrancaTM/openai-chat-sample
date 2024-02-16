93be54a84cdd4944b80feeb4d6303a1dbfefc2f2
FrancaTM <francatm@outlook.com>
2024-02-16 20:10:24 -0300

    add app loop

--- app.py
+++ app.py
@@ -5,18 +5,19 @@ client = OpenAI(api_key=api_key.api_key)
 
 chat_messages = []
 
-user_message = input("User: ")
+while True:
+    user_message = input("User: ")
 
-chat_messages.append({"role": "user", "content": user_message})
+    chat_messages.append({"role": "user", "content": user_message})
 
-response = client.chat.completions.create(
-    model="gpt-4-turbo-preview",
-    messages=chat_messages,
-    temperature=0.2,
-    max_tokens=10,
-)
+    response = client.chat.completions.create(
+        model="gpt-4-turbo-preview",
+        messages=chat_messages,
+        temperature=0.2,
+        max_tokens=10,
+    )
 
-ai_response = response.choices[0].message.content
-chat_messages.append({"role": "assistant", "content": ai_response})
-print("AI: ", ai_response)
-print(chat_messages)
+    ai_response = response.choices[0].message.content
+    chat_messages.append({"role": "assistant", "content": ai_response})
+    print("\nAI: ", ai_response, "\n")
+    # print(chat_messages, "\n")
