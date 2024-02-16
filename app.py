b946a8a7657f4aefad0ef0179b146cad31d24295
FrancaTM <francatm@outlook.com>
2024-02-16 19:19:01 -0300

    add user input change gpt model

--- app.py
+++ app.py
@@ -3,11 +3,13 @@ import api_key
 
 client = OpenAI(api_key=api_key.api_key)
 
+user_input = input("Add prompt: ")
+
 completion = client.chat.completions.create(
-    model="gpt-3.5-turbo",
+    model="gpt-4-turbo-preview",
     messages=[
-        {"role": "system", "content": "You are a helpful assistant."},
-        {"role": "user", "content": "Hello!"},
+        # {"role": "system", "content": "You are a helpful assistant."},
+        {"role": "user", "content": user_input},
     ],
 )
 
