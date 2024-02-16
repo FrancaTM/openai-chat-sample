47d102446a15051071cd0835d7bb5829b08e646f
FrancaTM <francatm@outlook.com>
2024-02-16 19:09:24 -0300

    add starter sample

--- app.py
+++ app.py
@@ -1,1 +1,14 @@
-print("hello")
+from openai import OpenAI
+import api_key
+
+client = OpenAI(api_key=api_key.api_key)
+
+completion = client.chat.completions.create(
+    model="gpt-3.5-turbo",
+    messages=[
+        {"role": "system", "content": "You are a helpful assistant."},
+        {"role": "user", "content": "Hello!"},
+    ],
+)
+
+print(completion.choices[0].message)
