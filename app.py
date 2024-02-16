6d45e18f16d44f3bef95fde9125949fd83d6daaa
FrancaTM <francatm@outlook.com>
2024-02-16 19:54:07 -0300

    add support for multiple messages

--- app.py
+++ app.py
@@ -3,16 +3,20 @@ import api_key
 
 client = OpenAI(api_key=api_key.api_key)
 
+chat_messages = []
+
 user_message = input("User: ")
+
+chat_messages.append({"role": "user", "content": user_message})
 
 response = client.chat.completions.create(
     model="gpt-4-turbo-preview",
-    messages=[
-        {"role": "user", "content": user_message},
-    ],
+    messages=chat_messages,
     temperature=0.2,
-    max_tokens=50,
+    max_tokens=10,
 )
 
 ai_response = response.choices[0].message.content
+chat_messages.append({"role": "assistant", "content": ai_response})
 print("AI: ", ai_response)
+print(chat_messages)
