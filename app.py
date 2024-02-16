48e06e2d53271d762b111eb7b7fd5645cac0ecbd
FrancaTM <francatm@outlook.com>
2024-02-16 19:40:14 -0300

    minor changes

--- app.py
+++ app.py
@@ -3,15 +3,16 @@ import api_key
 
 client = OpenAI(api_key=api_key.api_key)
 
-user_input = input("Add prompt: ")
+user_message = input("User: ")
 
-completion = client.chat.completions.create(
+response = client.chat.completions.create(
     model="gpt-4-turbo-preview",
     messages=[
-        {"role": "user", "content": user_input},
+        {"role": "user", "content": user_message},
     ],
     temperature=0.2,
     max_tokens=50,
 )
 
-print(completion.choices[0].message)
+ai_response = response.choices[0].message.content
+print("AI: ", ai_response)
